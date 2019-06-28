66fe72a0511c97ab29721485ee6927e1ac1f5600
Chumicat <russell57260620@gmail.com>
2019-06-28 13:53:55 +0800

    v1.01

---
+++ GameProject/Game5.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using WMPLib;
+
+
+
+namespace GameProject
+{
+    public partial class Game5 : Form
+    {
+        Button[,] buttons = new Button[DEFINE.COL_SIZE, DEFINE.ROW_SIZE];
+        int[,] plane = new int[DEFINE.COL_SIZE, DEFINE.ROW_SIZE];
+        Random rnd = new Random();
+        int t_conumter = 0;
+        int remaining_card; // unit is pair
+        SoundPlayer sound_true = new SoundPlayer(DEFINE.S_TRUE);
+        SoundPlayer sound_false = new SoundPlayer(DEFINE.S_FALSE);
+        WindowsMediaPlayer wmp = new WindowsMediaPlayer();
+
+        public Game5()
+        {
+            InitializeComponent();
+        }
+
+        private void Game5_Load(object sender, EventArgs e)
+        {
+            // background music setting
+            wmp.URL = "background.mp3";
+
+            // genetrate button
+            for (int y = 0; y < DEFINE.ROW_SIZE; y++)
+            {
+                for (int x = 0; x < DEFINE.COL_SIZE; x++)
+                {
+                    buttons[x, y] = new Button();
+                    buttons[x, y].Location = new Point(x * (this.Width - DEFINE.WIDTH_GAP)/DEFINE.COL_SIZE, y * (this.Height - DEFINE.HEIGHT_GAP)/DEFINE.ROW_SIZE);
+                    this.Controls.Add(buttons[x, y]);
+                    buttons[x, y].Size = new Size((this.Width - DEFINE.WIDTH_GAP) / DEFINE.COL_SIZE, (this.Height - DEFINE.HEIGHT_GAP) / DEFINE.ROW_SIZE);
+                    buttons[x, y].Click += new EventHandler(this.Button_Click);
+                    buttons[x, y].Text = Convert.ToString(index(x, y));
+                    buttons[x, y].Name = Convert.ToString(index(x, y));
+                    plane[x, y] = index(x, y)%(DEFINE.COL_SIZE* DEFINE.ROW_SIZE/2);
+                    //Text = Convert.ToString(buttons[x, y].Size.ToString());
+                    
+                }
+            }
+
+            // shuffle the card
+            shuffle();
+
+            // initial the counter
+            remaining_card = DEFINE.COL_SIZE * DEFINE.ROW_SIZE / 2;
+
+            // initial the timer
+            t_conumter = 0;
+            timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Enabled = true;
+            return;
+        }
+
+        private int index(int x, int y)
+        {
+            return y * DEFINE.COL_SIZE + x;
+        }
+
+        private void shuffle() {
+            // genetrate number to each card
+            for (int n = 0; n < DEFINE.SHUFFLE_TIMES; n++)
+                Tools.Swap<int>(ref plane[rnd.Next() % DEFINE.COL_SIZE, rnd.Next() % DEFINE.ROW_SIZE],
+                                ref plane[rnd.Next() % DEFINE.COL_SIZE, rnd.Next() % DEFINE.ROW_SIZE]);
+
+            // tmp part to make num print on screem
+            for (int y = 0; y < DEFINE.ROW_SIZE; y++)
+                for (int x = 0; x < DEFINE.COL_SIZE; x++)
+                    buttons[x, y].Text = Convert.ToString(plane[x, y]);
+
+            return;
+        }
+
+        bool se = false;    // selected boolean
+        Button target;      // selected button reference
+        private void Button_Click(Object sender, EventArgs e)
+        {
+            Button this_sender = (Button)sender;
+
+            if (!se) {
+                // first time situation
+                Text = "";
+                target = this_sender;
+                se = true;
+            }else if (this_sender.Equals(target)){
+                // select same situation (do nothing)
+            }else if (Convert.ToInt32(target.Text) == Convert.ToInt32(this_sender.Text)){
+                // correct situation
+                target.Hide();
+                this_sender.Hide();
+                remaining_card--;
+                sound_true.Play();
+                se = false;
+                // end of game
+                if (remaining_card == 0) {
+                    this.Hide();
+                    Parent.Show();
+                }
+            }else{
+                // false situation
+                sound_false.Play();
+                se = false;
+            }
+            return;
+        }
+
+        private void reset() {
+            // show all of the buttons
+            for (int y = 0; y < DEFINE.ROW_SIZE; y++)
+                for (int x = 0; x < DEFINE.COL_SIZE; x++)
+                    buttons[x, y].Show();
+
+            // shuffle the cards position
+            shuffle();
+
+            // initial the counter
+            remaining_card = DEFINE.COL_SIZE * DEFINE.ROW_SIZE / 2;
+
+            // reenable timer.
+            t_conumter = 0;
+            timer1.Enabled = true;
+            return;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //t_conumter++;
+            //Text = Convert.ToString(t_conumter);
+        }
+
+        private void Game5_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            wmp.close();
+        }
+    }
+
+    static class DEFINE
+    {
+        public const int ROW_SIZE = 4;
+        public const int COL_SIZE = 8;
+        public const int WIDTH_GAP = 16;
+        public const int HEIGHT_GAP = 38;
+        public const int SHUFFLE_TIMES = 200;
+        public const String S_TRUE = "../../Resources/sound/true.wav";
+        public const String S_FALSE = "../../Resources/sound/false.wav";
+        public const String M_BACKGROUND = "../../Resources/music/background.mp3";
+    }
+
+    static class Tools
+    {
+        public static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+    }
+}
---
+++ GameProject/ScoreBoard.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace GameProject
+{
+    partial class ScoreBoard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.l_Scores = new System.Windows.Forms.Label();
+            this.r_Scores = new System.Windows.Forms.Label();
+            this.l_Time = new System.Windows.Forms.Label();
+            this.r_Time = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(278, 245);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 178);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(272, 64);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(139, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.l_Scores, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.r_Scores, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.l_Time, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.r_Time, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 2);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(272, 169);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // l_Scores
+            // 
+            this.l_Scores.AutoSize = true;
+            this.l_Scores.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.l_Scores.Font = new System.Drawing.Font("標楷體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.l_Scores.Location = new System.Drawing.Point(3, 0);
+            this.l_Scores.Name = "l_Scores";
+            this.l_Scores.Size = new System.Drawing.Size(130, 56);
+            this.l_Scores.TabIndex = 0;
+            this.l_Scores.Text = "Scores";
+            // 
+            // r_Scores
+            // 
+            this.r_Scores.AutoSize = true;
+            this.r_Scores.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.r_Scores.Font = new System.Drawing.Font("標楷體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.r_Scores.Location = new System.Drawing.Point(139, 0);
+            this.r_Scores.Name = "r_Scores";
+            this.r_Scores.Size = new System.Drawing.Size(130, 56);
+            this.r_Scores.TabIndex = 1;
+            // 
+            // l_Time
+            // 
+            this.l_Time.AutoSize = true;
+            this.l_Time.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.l_Time.Font = new System.Drawing.Font("標楷體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.l_Time.Location = new System.Drawing.Point(3, 56);
+            this.l_Time.Name = "l_Time";
+            this.l_Time.Size = new System.Drawing.Size(130, 56);
+            this.l_Time.TabIndex = 2;
+            this.l_Time.Text = "Time";
+            // 
+            // r_Time
+            // 
+            this.r_Time.AutoSize = true;
+            this.r_Time.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.r_Time.Font = new System.Drawing.Font("標楷體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.r_Time.Location = new System.Drawing.Point(139, 56);
+            this.r_Time.Name = "r_Time";
+            this.r_Time.Size = new System.Drawing.Size(130, 56);
+            this.r_Time.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 57);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // ScoreBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 245);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ScoreBoard";
+            this.Text = "ScoreBoard";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label l_Scores;
+        private System.Windows.Forms.Label r_Scores;
+        private System.Windows.Forms.Label l_Time;
+        private System.Windows.Forms.Label r_Time;
+        private System.Windows.Forms.Label label1;
+    }
+}
